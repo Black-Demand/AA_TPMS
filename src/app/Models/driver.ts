04b218b8b8b813d17b82194d13d2f7a5b537284a
Henok <enochhenok16@gmail.com>
2025-05-07 01:43:52 -0700

    remove dashboard

--- src/app/Models/driver.ts
+++ src/app/Models/driver.ts
@@ -23,6 +23,7 @@ export interface DriverDTO {
   woreda?: string | null;
   kebele?: string | null;
   remark?: string | null;
+  issuanceDate?: string | null;
 
   // issuanceDate?: string | null; // Date string in ISO format
   // town?: string | null;
