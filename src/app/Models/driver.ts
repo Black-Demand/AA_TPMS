0a71296f0ee254f15c3f93eae290b3443276516e
henok <enochhenok16@gmail.com>
2025-06-04 09:56:47 +0300

    added traffic

--- src/app/Models/driver.ts
+++ src/app/Models/driver.ts
@@ -1,4 +1,5 @@
 export interface DriverDTO {
+  fullName: any;
   licenseNumber: any;
   licenseCategory: any;
   issuerRegion: any;
--- src/app/driver/traffic/traffic.component.ts
+++ src/app/driver/traffic/traffic.component.ts
@@ -1,26 +1,25 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
-import { dateCannotBeTheFuture, minAgeValidator } from '../age.validate';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
  
 // Material Modules
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatCheckboxModule} from '@angular/material/checkbox';
 import { MatSelectModule } from '@angular/material/select';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatTimepickerModule } from '@angular/material/timepicker'
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatRadioModule } from "@angular/material/radio";
-import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatTabsModule } from '@angular/material/tabs';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import Lookup from '../../Models/lookup';
-import { privateDecrypt } from 'crypto';
 import { LookupService } from '../../services/lookup.service';
 import { Gender, GenderDescriptions } from '../../Enums/gender';
 import { DriverDTO } from '../../Models/driver';
@@ -29,17 +28,10 @@ import { ToastrService } from 'ngx-toastr';
 import { MatDividerModule } from '@angular/material/divider';
 import { Router } from '@angular/router';
 import { dateNotTheFuture, dateNotTheFutures } from '../../service/date.validate';
-import { MatSort } from '@angular/material/sort';
-interface ViolationRecord {
-  fullName: string;
-  licenseId: string;
-  ticketNumber: string;
-  violationDate: Date;
-  violationGrade: string;
-}
+
 @Component({
   selector: 'app-traffic',
-   imports: [
+  imports: [
     CommonModule,
     ReactiveFormsModule,
     MatFormFieldModule,
@@ -56,29 +48,19 @@ interface ViolationRecord {
     MatStepperModule,
     MatTabsModule,
     MatDividerModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatTimepickerModule
   ],
   templateUrl: './traffic.component.html',
   styleUrl: './traffic.component.css'
 })
-export class TrafficComponent implements OnInit {
+export class TrafficComponent  implements OnInit {
 
-  registrationForm: FormGroup;
-  imagePreview: string | ArrayBuffer | null = null;
-  selectedPhotoFile: File | null = null;
-  displayedColumns: string[] = ['fullName', 'licenseId', 'ticketNumber', 'violationDate', 'violationGrade', 'actions'];
-  dataSource!: MatTableDataSource<ViolationRecord>;
-
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
-  
-  
-  genderEnum = Gender;
-  genderOptions = [
-    { value: Gender.Male, label: GenderDescriptions[Gender.Male] },
-    { value: Gender.Female, label: GenderDescriptions[Gender.Female] }
-  ];
-  regions: Lookup.RegionDTO[] = [];
+  trafficForm!: FormGroup;
+  violationTypeDisabled = false;
+  violations: any[] = [];
+  displayedColumns: string[] = ['fullName', 'licenseNumber', 'ticket', 'dateAccused'];
+ 
   zones: Lookup.ZoneDTO[] = [];
   woredas: Lookup.WoredaDTO[] = [];
   kebeles: Lookup.KebeleDTO[] = [];
@@ -86,20 +68,21 @@ export class TrafficComponent implements OnInit {
   licenceAreas: Lookup.LicenceAreaDTO[] = [];
   licenceCatagories: Lookup.LicenceCategoryDTO[] = [];
   nationalities: Lookup.LookupDTO[] = [];
-  
 
   selectedRegionCode!: number;  
   selectedZoneCode!: number;
   selectedWoredaCode!: number;
   selectedLicenceRegionCode: number = 0;
-
-violationgrades: Lookup.OffenceGradeDTO[] =[];
-violationtypes: Lookup.OffenceNewDTO[] =[];
- violationTypeDisabled = false;
-checked: any;
-labelPosition: any;
+  selectedDriver!: DriverDTO;
 
 
+  
+  violationgrades: Lookup.OffenceGradeDTO[] = [];
+  violationtypes: Lookup.OffenceNewDTO[] = [];
+  regions: Lookup.LicenceRegionDTO[] = [];
+  majors: Lookup.Majors[] = [];
+
+  
 
   constructor(
     private fb: FormBuilder,
@@ -108,23 +91,76 @@ labelPosition: any;
     private toastr: ToastrService,
     private router: Router
   ) {
-    this.registrationForm = this.fb.group({
-      fullInfo: ['', Validators.required],
-      violationDate: new FormControl<Date | null>(null, [Validators.required, dateNotTheFuture()]),
-      dateAccused: new FormControl<Date | null>(null, [Validators.required, dateNotTheFutures()]),
+
+  }
+
+    ngOnInit(): void {
+    this.loadRegions();
+    this.loadMajors();
+    this.loadViolationGrade();
+    const data = this.tdrs.getDriverData();
+    this.trafficForms();
+    this.generateOrdderNumber();
+   
+
+    if(data){
+      this.selectedDriver = data;
+      this.trafficForm.patchValue({
+        mainGuid: data.mainGuid,
+        fullName: data.fullName,
+        licenseNumber: data.licenseNumber
+      })
+    }
+  }
+
+  trafficForms(): void {
+    
+    this.trafficForm = this.fb.group({
+      fullName: [{value: '', disabled: true}, Validators.required],
+      licenseNumber: [{value: '', disabled: true}, Validators.required],
       violationGrade: ['', Validators.required],
-      placeAccused: ['', Validators.required],
       offenceId: [{ value: '', disabled: false }, Validators.required],
-      payment: ['', Validators.required],
-      ticket: ['', Validators.required],
+      violationDate: new FormControl<Date | null>(null, [Validators.required, dateNotTheFuture()]),
       tName: ['', Validators.required],
-      actionTaken: ['', Validators.required],
+      action: ['', Validators.required],
       plateRegion: ['', Validators.required],
       NewPlateCode: ['', Validators.required],
-      NewPlateNo: ['', Validators.required]
+      NewPlateNo: ['', Validators.required],
+      placeAccused: ['', Validators.required],
+      dateAccused: new FormControl<Date | null>(null, [Validators.required, dateNotTheFutures()]),
+      payment: ['', Validators.required],
+      ticket: ['', Validators.required],
+      vehicleType: ['', Validators.required],
+      timeAccused: ['', Validators.required],
+      damageAssessment: this.fb.group({
+        simpleBodyDamage: [false], heavyBodyDamage: [false], materialDamage: [false]
+  })
+
     });
   }
 
+    loadViolationGrade() {
+    this.lookupservice.getAllOffences().subscribe(data => {
+       this.violationgrades = data;
+    });
+  }
+
+ loadViolationTypeByGrade(gradeCode: string) {
+    const numericGradeCode = Number(gradeCode);
+  
+    if (numericGradeCode > 3) {
+      this.violationTypeDisabled = true;
+      this.trafficForm.get('violationType')?.reset();
+      this.trafficForm.get('violationType')?.disable();
+      this.violationtypes = [];
+    } else {
+      this.violationTypeDisabled = false;
+      this.trafficForm.get('violationType')?.enable();
+      this.lookupservice.getAllOffenceNew([gradeCode]).subscribe(data => {
+        this.violationtypes = data;
+      });
+    }
+  }
 
     dateRangeValidator(group: AbstractControl): ValidationErrors | null {
     const yetKen = group.get('violationDate')?.value;
@@ -137,13 +173,10 @@ labelPosition: any;
 }
 
   get minEndDate(): Date | null {
-    return this.registrationForm.get('violationDate')?.value;
+    return this.trafficForm.get('violationDate')?.value;
   }
-
-
-
-   getErrorMessageForYetfesmbetKen():string{
-    let field = this.registrationForm.get('violationDate');
+  getErrorMessageForYetfesmbetKen():string{
+    let field = this.trafficForm.get('violationDate');
     
     if(field?.hasError('required')){
       return 'The field is Required';
@@ -153,8 +186,10 @@ labelPosition: any;
     }
     return "";
   }
-  getErrorMessageForYetfessmbetKen():string{
-    let field = this.registrationForm.get('dateAccused');
+
+
+   getErrorMessageForYetfessmbetKen():string{
+    let field = this.trafficForm.get('dateAccused');
     
     if(field?.hasError('required')){
       return 'The field is Required';
@@ -162,239 +197,62 @@ labelPosition: any;
     if(field?.hasError('dateNotTheFuture')){
       return field.getError('dateNotTheFuture').message;
     }
-    if(this.registrationForm.hasError('dateRangeInvalid')) {
+    if(this.trafficForm.hasError('dateRangeInvalid')) {
       return 'Yetkessbte Ken must be greater than or equal to Yetfesmbte Ken';
     }
     return "";
   }
 
-  loadViolationGrade() {
-    this.lookupservice.getAllOffences().subscribe(data => {
-       this.violationgrades = data;
-    });
-  }
+    generateOrdderNumber(){
+    const randomNum = Math.floor(100000 + Math.random() * 900000);
+    const orderNumber = `${randomNum}`;
 
-  loadViolationTypeByGrade(gradeCode: string) {
-    const numericGradeCode = Number(gradeCode);
-  
-    if (numericGradeCode > 3) {
-      this.violationTypeDisabled = true;
-      this.registrationForm.get('violationType')?.reset();
-      this.registrationForm.get('violationType')?.disable();
-      this.violationtypes = [];
-    } else {
-      this.violationTypeDisabled = false;
-      this.registrationForm.get('violationType')?.enable();
-      this.lookupservice.getAllOffenceNew([gradeCode]).subscribe(data => {
-        this.violationtypes = data;
-      });
-    }
+    this.trafficForm?.get('ticket')?.setValue(orderNumber);
+    this.trafficForm?.get('ticket')?.disable();
   }
+ 
 
-  onPhoneInput(event: Event): void {
-    const input = event.target as HTMLInputElement;
-    let value = input.value.replace(/\D/g, ''); // Remove non-digit characters
-    
-    // Ensure we don't exceed max length
-    if (value.length > 9) {
-      value = value.substring(0, 9);
-    }
-    
-    input.value = value;
-    this.registrationForm.get('phone')?.setValue(value, { emitEvent: false });
-  }
-
-  getFullPhoneNumber(): string {
-    const phoneControl = this.registrationForm.get('phone');
-    return phoneControl?.value ? `+251${phoneControl.value}` : '';
-  }
-
-  get licenseNumberControl() {
-    return this.registrationForm.get('licenseNumber');
-  }
-
-  getErrorForLicenseNumber(): string {
-    if (this.licenseNumberControl?.hasError('required')) {
-      return 'Driver\'s License Number is required';
-    }
-    if (this.licenseNumberControl?.hasError('minlength') || 
-        this.licenseNumberControl?.hasError('maxlength')) {
-      return 'Must be exactly 6 characters';
-    }
-    return '';
-  }
-
-  getErrorForIssueDate(): string {
-    const field = this.registrationForm.get('issuanceDate');
-  
-    if (field?.hasError('required')) {
-      return 'The issue date is required';
-    }
-  
-    if (field?.hasError('dateCannotBeTheFuture')) {
-      return 'Issue date cannot be in the future';
-    }
-  
-    return '';
-  }
-
-  ngOnInit(): void {
-    this.loadRegions();
-    this.loadCategories();
-    this.loadLicenceRegions();
-    this.loadNationality();
-        this.loadViolationGrade();
-
-  }
-   
-  onRegionChange(regionCode: number) {
-    this.selectedRegionCode = regionCode; 
-    this.loadZones(regionCode);
-  }
-  
-  onZoneChange(zoneCode: number) {
-    console.log('Zone changed:', zoneCode);
-    this.selectedZoneCode = zoneCode;
-    this.loadWoredas(zoneCode);
-  }
-  
-
-  onWoredaChange(woredaCode: number) {
-    console.log('Woreda changed:', woredaCode);
-    this.selectedWoredaCode = woredaCode;
-    this.loadKebeles(woredaCode);
-  }
-  
-
-
-  loadRegions() {
-    this.lookupservice.getRegions().subscribe(data => {
-      this.regions = data;
-      this.zones = [];
-      this.woredas = [];
-      this.kebeles = [];
-    });
-  }
-  
-  loadZones(regionCode: number) {
-    this.lookupservice.getZonesByRegion(regionCode).subscribe(data => {
-      this.zones = data;
-      this.woredas = [];
-      this.kebeles = [];
-    });
-  }
-  
-  loadWoredas(zoneCode: number) {
-    this.lookupservice.getWoredasByZone(zoneCode).subscribe(data => {
-      this.woredas = data;
-      this.kebeles = [];
-    });
-  }
-  
-  
-  loadKebeles(woredaCode: number) {
-    this.lookupservice.getKebelesByWoreda(woredaCode).subscribe(data => {
-      this.kebeles = data;
-    });
-  }
-
-  loadLicenceRegions(): void {
+   loadRegions() {
     this.lookupservice.getAllRegions().subscribe(data => {
-      console.log('Loaded Regions:', data);
-      this.licenceRegions = data;
+       this.regions = data;
     });
   }
-  onIssuerRegionChange(regionCode: number): void {
-    console.log('Selected Region Code:', regionCode); 
-    if (regionCode != null) {
-      this.selectedLicenceRegionCode = regionCode;
-      this.loadAreasByLicenceRegion(regionCode);
-    }
-  }
-  
-  loadAreasByLicenceRegion(regionCode: number): void {
-    this.lookupservice.getAllAreas(regionCode).subscribe(data => {
-      this.licenceAreas = data;
+
+  loadMajors() {
+    this.lookupservice.getAllMajors().subscribe(data => {
+       this.majors = data;
     });
   }
   
-  
-
-  loadCategories() {
-    this.lookupservice.getAllCategories().subscribe(data => {
-      this.licenceCatagories = data;
-    })
-  }
-  loadNationality() {
-    this.lookupservice.getAllNationality().subscribe(
-      data => {
-        this.nationalities = data;
-      }
-    );
-  }
-
-
-
-  
-
-  onFileSelected(event: Event): void {
-    const input = event.target as HTMLInputElement;
-    if (input.files && input.files.length > 0) {
-      const file = input.files[0];
-      
-      // Validate file type
-      if (!['image/jpeg', 'image/png'].includes(file.type)) {
-        this.registrationForm.get('photo')?.setErrors({ fileType: true });
-        return;
-      }
-      
-      // Validate file size (5MB)
-      if (file.size > 5 * 1024 * 1024) {
-        this.registrationForm.get('photo')?.setErrors({ fileSize: true });
-        return;
-      }
-      
-      // Update form control
-      this.selectedPhotoFile = file;
-      this.registrationForm.patchValue({ photo: file });
-      this.registrationForm.get('photo')?.updateValueAndValidity();
-      
-      // Create preview
-      const reader = new FileReader();
-      reader.onload = () => {
-        this.imagePreview = reader.result;
-      };
-      reader.readAsDataURL(file);
-    }
-  }
-
   onSubmit(): void {
-    console.log(this.registrationForm.value);
-  
-    if (this.registrationForm.valid) {
-      const driver: DriverDTO = this.registrationForm.value;
-  
-      this.tdrs.createDriver(driver).subscribe({
-        next: (response) => {
-          // alert('Driver created successfully');
-          this.toastr.success("Driver created successfully");
-        //this.router.navigate(['/penality']);
-        },
-        error: (err) => {
-          console.error('Error creating driver:', err);
-          alert('Error: ' + (err.error?.message || err.message || 'Unknown error'));
-          this.toastr.error("'Error creating driver:" , err);
-        }
-      });
-    } else {
-      this.registrationForm.markAllAsTouched(); // Highlight validation errors
-    }
+  if (this.trafficForm.valid) {
+    // Get all values including disabled fields
+    const formValue = {
+      ...this.trafficForm.getRawValue(), // This includes disabled fields
+      fullName: this.selectedDriver.fullName,
+      licenseNumber: this.selectedDriver.licenseNumber
+    };
+
+    // Add to violations array for the table
+    this.violations.push(formValue);
+    
+    console.log('Violation submitted:', formValue);
+
+    this.tdrs.createDriver(formValue).subscribe({
+      next: (response) => {
+        this.toastr.success("Driver created successfully");
+      },
+      error: (err) => {
+        console.error('Error creating driver:', err);
+        this.toastr.error("Error creating driver:", err);
+      }
+    });
+  } else {
+    this.trafficForm.markAllAsTouched();
   }
-  
+}
   
   onReset(): void {
-    this.registrationForm.reset();
-    this.imagePreview = null;
-    this.selectedPhotoFile = null;
+    this.trafficForm.reset();
   }
 }
